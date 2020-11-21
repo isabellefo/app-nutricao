1f1ab2293a20be38c69a6bce555ed1a723be1545
Gabriel Mendes <gabriel.mendespng@gmail.com>
2020-11-21 19:08:22 -0300

    Substitui lista de ingredientes por um hashmap e implementa método toString

--- app-nutricao/src/nutri/Receita.java
+++ app-nutricao/src/nutri/Receita.java
@@ -1,37 +1,59 @@
 package nutri;
 
-import java.util.List;
+import java.util.Map;
 
 public class Receita {
 	
 	private String nome;
-	private List<Alimento>ingredientes;
+	private Map<Alimento, Integer> ingredientes; // alimento e quantidade
 	private String tempo;
 	private String preparo;
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	public List<Alimento> getIngredientes() {
+
+	public Map<Alimento, Integer> getIngredientes() {
 		return ingredientes;
 	}
-	public void setIngredientes(List<Alimento> ingredientes) {
+
+	public void setIngredientes(Map<Alimento, Integer> ingredientes) {
 		this.ingredientes = ingredientes;
 	}
+
 	public String getTempo() {
 		return tempo;
 	}
+
 	public void setTempo(String tempo) {
 		this.tempo = tempo;
 	}
+
 	public String getPreparo() {
 		return preparo;
 	}
+
 	public void setPreparo(String preparo) {
 		this.preparo = preparo;
 	}
 
+	public String toString() {
+		String descricao = "";
+
+		descricao += nome.toUpperCase() + "\n";
+		descricao += "Tempo de preparo: " + tempo + "\n\n";
+		descricao += "Ingredientes:\n";
+		for (var entry: ingredientes.entrySet()) {
+			descricao += String.format("  %d %s\n", entry.getValue(), entry.getKey().getNome());
+		}
+
+		descricao += "\nModo de preparo:\n" + preparo;
+
+		return descricao;
+	}
 
 }
