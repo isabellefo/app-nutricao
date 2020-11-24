9c16d9ee085bb1acda3c32d458207d5a6551d859
Gabriel Borges <gabriel.borges@solucx.com.br>
2020-11-23 23:46:52 -0300

    correcao de bugs

--- app-nutricao/src/menu/App.java
+++ app-nutricao/src/menu/App.java
@@ -3,6 +3,7 @@ package menu;
 
 import cadastros.CadastroAlimento;
 import cadastros.CadastroCombinacao;
+import cadastros.CadastroReceita;
 import database.Database;
 
 public class App {
@@ -10,9 +11,13 @@ public class App {
 	static Database db = Database.getDatabase();
 	static CadastroAlimento cadastroAlimento = new CadastroAlimento(db.getAlimentos());
 	static CadastroCombinacao cadastroCombinacao = new CadastroCombinacao();
+	static CadastroReceita cadastroReceita = new CadastroReceita();
 	
 	public static void main(String[] args) {
-		
+		cadastroAlimento.cadastrar();
+		cadastroAlimento.cadastrar();
+		cadastroCombinacao.cadastrar();
+		cadastroReceita.cadastrar();
 	}
 
 }
